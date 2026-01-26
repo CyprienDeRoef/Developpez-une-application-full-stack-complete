dc061631655590979cb9be61ae625837bb44b1b7
Cyprien De Roef <c.deroef@outlook.fr>
2026-01-26 15:29:36 +0100

    feat: ajout de la page /topics

---
+++ front/src/app/pages/topics/topics.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { TopicService, Topic } from '../../services/topic.service';
+
+@Component({
+  selector: 'app-topics',
+  templateUrl: './topics.component.html',
+  styleUrls: ['./topics.component.scss'],
+})
+export class TopicsComponent implements OnInit {
+  topics: Topic[] = [];
+  subscribedTopicIds: Set<number> = new Set();
+  isLoading: boolean = true;
+
+  constructor(private topicService: TopicService) {}
+
+  ngOnInit(): void {
+    this.loadTopics();
+    this.loadSubscriptions();
+  }
+
+  loadTopics(): void {
+    this.topicService.all().subscribe({
+      next: (topics) => {
+        this.topics = topics;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading topics:', error);
+        this.isLoading = false;
+      },
+    });
+  }
+
+  loadSubscriptions(): void {
+    this.topicService.getUserSubscriptions().subscribe({
+      next: (subscribedTopics) => {
+        this.subscribedTopicIds = new Set(
+          subscribedTopics.map((topic) => topic.id)
+        );
+      },
+      error: (error) => {
+        console.error('Error loading subscriptions:', error);
+      },
+    });
+  }
+
+  isSubscribed(topicId: number): boolean {
+    return this.subscribedTopicIds.has(topicId);
+  }
+
+  toggleSubscription(topic: Topic): void {
+    if (this.isSubscribed(topic.id)) {
+      this.unsubscribe(topic);
+    } else {
+      this.subscribe(topic);
+    }
+  }
+
+  subscribe(topic: Topic): void {
+    this.topicService.subscribe(topic.id.toString()).subscribe({
+      next: () => {
+        this.subscribedTopicIds.add(topic.id);
+      },
+      error: (error) => {
+        console.error('Error subscribing:', error);
+      },
+    });
+  }
+
+  unsubscribe(topic: Topic): void {
+    this.topicService.unsubscribe(topic.id.toString()).subscribe({
+      next: () => {
+        this.subscribedTopicIds.delete(topic.id);
+      },
+      error: (error) => {
+        console.error('Error unsubscribing:', error);
+      },
+    });
+  }
+}
