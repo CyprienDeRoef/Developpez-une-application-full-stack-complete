af09e075e298015843c535df3e81355e86db5556
Cyprien De Roef <c.deroef@outlook.fr>
2026-01-26 15:28:55 +0100

    feat: ajout de liens et d'options dans le header

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+  public isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  public logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+}
